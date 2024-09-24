c2e9f9acfa66604dd1861f2194e4469e02371b94
Eslamadelelkhouly <rodyerak@gmail.com>
2024-09-25 01:55:03 +0300

    add views in peroperty

--- property/views.py
+++ property/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.views.generic import ListView , DetailView
+
+class PropertyList(ListView):
+    pass
+    # filter
+
+
+class PropertyDetail(DetailView):
+    pass
+    ## Book
