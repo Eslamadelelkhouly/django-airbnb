1af22f88e17a20a7ac3714c9dbed02d04920b34f
Eslamadelelkhouly <rodyerak@gmail.com>
2024-09-24 21:04:37 +0300

    add models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.utils import timezone
+from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 # Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(User, verbose_name="post_author", on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
+    tags = TaggableManager()
+    image = models.ImageField(upload_to='post/')
+    created_At = models.DateTimeField(default=timezone.now)
+    description = models.TextField(max_length=15000)
+    category = models.ForeignKey('Category', verbose_name="post_category", on_delete=models.CASCADE)
+    def __str__(self):
+            return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+            return self.name
+    
--- property/models.py
+++ property/models.py
@@ -1,3 +1,68 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+class Property(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='property/')
+    price = models.IntegerField(default=0)
+    description = models.TextField(max_length=10000)
+    place = models.ForeignKey("Place", on_delete=models.CASCADE, verbose_name="property_place", related_name='properties')
+    category = models.ForeignKey("Category", on_delete=models.CASCADE, verbose_name="property_category", related_name='properties')
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
+
+
+class PropertyImages(models.Model):
+    property = models.ForeignKey(Property, on_delete=models.CASCADE, verbose_name="property image", related_name='images')
+    image = models.ImageField(upload_to='propertyimages/')
+    
+    def __str__(self):
+        return str(self.property)
+
+
+class Place(models.Model):
+    name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='places/')
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+
+
+class PropertyReview(models.Model):  # Fixed typo here
+    author = models.ForeignKey(User, verbose_name="review_author", on_delete=models.CASCADE)
+    property = models.ForeignKey(Property, verbose_name="review_property", on_delete=models.CASCADE)  # Fixed typo here
+    rate = models.IntegerField(default=0)
+    feedback = models.TextField(max_length=2000)
+    created_at = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return str(self.property)  # Fixed typo here
+
+COUNT = (
+    (1,1),
+    (2,1),
+    (3,1),
+    (4,1),
+)
+
+class PropertyBook(models.Model):  # Fixed typo here
+    user = models.ForeignKey(User, verbose_name="book_owner", on_delete=models.CASCADE)
+    property = models.ForeignKey(Property, verbose_name="book_property", on_delete=models.CASCADE)  # Fixed typo here
+    date_from = models.DateField(default = timezone.now)
+    date_to = models.DateField(default = timezone.now)
+    guest = models.CharField(max_length= 2 , choices=COUNT)
+    children = models.CharField(max_length= 2 , choices=COUNT)
+
+    def __str__(self):
+        return str(self.property)
