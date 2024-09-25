dd90fdf0d276d907bdfccd360d566c47771f27c1
Eslamadelelkhouly <rodyerak@gmail.com>
2024-09-25 19:03:42 +0300

    add static files

--- property/views.py
+++ property/views.py
@@ -3,9 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.views.generic import ListView , DetailView
+from .models import Property
 
 class PropertyList(ListView):
-    pass
+    model = Property
     # filter
 
 
