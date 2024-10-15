19559354f5563aaea7e090d98b2f7fba362c3621
Eslamadelelkhouly <rodyerak@gmail.com>
2024-10-16 01:17:01 +0300

    add places in search in home

--- settings/views.py
+++ settings/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
+from property .models import Property , Place
 # Create your views here.
 
 
 def home(request):
-    return render(request , 'settings/home.html',{})
+    places = Place.objects.all()
+    return render(request , 'settings/home.html',{'places':places},)
